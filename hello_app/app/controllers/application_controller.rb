079b1e4a749e375a05ce54635800263022aa4380
staticWagomU <to6wa17@gmail.com>
2024-06-09 22:05:04 +0900

    feat: hello world

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def hello
+    render html: "hello, ruby on rails tutorial!"
+  end
 end
--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # application_controller.rbのhelloをrootに設定する
+  root "application#hello"
 end
