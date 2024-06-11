d81931ef6c196c9d3af60849259af6598f06b684
staticWagomU <to6wa17@gmail.com>
2024-06-11 23:12:05 +0900

    feat: validate追加

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ApplicationRecord
+  # MicropostはUserに属している
+  belongs_to :user
+  # これだけでバリデートできるらしい
+  validates :content, length: { maximum: 140 },
+                      presence: true
 end
